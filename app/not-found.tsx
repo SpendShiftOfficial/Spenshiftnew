d68486b2f0650f9c678f7d58efa44eff0f17c766
SpendShiftOfficial <graypatrick441@gmail.com>
2026-06-11 16:23:36 +0500

    first-commit

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,7 @@
 export default function NotFound() {
   return (
     <div style={{padding:"100px",textAlign:"center"}}>
-      <h1>Report not found</h1>
+      <h1>Reports not found</h1>
       <p>This report may have expired or does not exist.</p>
     </div>
   );
