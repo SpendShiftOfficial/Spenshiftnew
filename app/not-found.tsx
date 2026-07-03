3377943c6e328ce0ec2c87752189685cb1819d30
SpendShiftOfficial <graypatrick441@gmail.com>
2026-07-04 00:15:20 +0500

    ss

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,11 +3,14 @@ import Header from "@/components/Header";
 
 export default function NotFound() {
   return (
+    <div>
+      <Header />
     
     <div style={{padding:"100px",textAlign:"center"}}>
-      <Header />
+      
       <h1>Reports not found</h1>
       <p>This report may have expired or does not exist.</p>
+    </div>
     </div>
   );
 }
